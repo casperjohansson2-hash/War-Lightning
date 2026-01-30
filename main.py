9ad64e0d5118fc159a1ec34b725046ffd1197cef
Casper Johansson <casper.johansson5@learnet.se>
2026-01-30 12:32:56 +0100

    New version

--- main.py
+++ main.py
@@ -33,11 +33,11 @@ def damage():
 
 background = pygame.image.load("C:/War Lightning/assets/tiles/svart.jpg")
 sprite_bullet = pygame.image.load("C:/War Lightning/assets/bullets/bullet.jpg")
-original_player1 = pygame.image.load("C:/War Lightning/assets/tanks/Player1.jpg")
-original_player2 = pygame.image.load("C:/War Lightning/assets/tanks/Player2.jpg")
+original_player1 = pygame.image.load("C:/War Lightning/assets/tanks/Player1.png")
+original_player2 = pygame.image.load("C:/War Lightning/assets/tanks/Player2.png")
 
-sprite_player1 = pygame.transform.scale(original_player1, (original_player1.get_width() // 2, original_player1.get_height() // 2))
-sprite_player2 = pygame.transform.scale(original_player2, (original_player2.get_width() // 2, original_player2.get_height() // 2))
+sprite_player1 = pygame.transform.smoothscale(original_player1, (original_player1.get_width(), original_player1.get_height()))
+sprite_player2 = pygame.transform.smoothscale(original_player2, (original_player2.get_width(), original_player2.get_height()))
 clock = pygame.time.Clock()
 
 game = True
@@ -52,7 +52,7 @@ class Player1:
         self.sprite_player1 = sprite_player1
         self.health = player_health
         self.damage = damage()
-        self.speed = 5
+        self.speed = 2
 
 
     def move(self):
@@ -75,7 +75,7 @@ class Player2:
         self.sprite_player2 = sprite_player2
         self.health = player_health
         self.damage = damage()
-        self.speed = 5
+        self.speed = 2
 
 
     def move(self):
@@ -94,23 +94,25 @@ class Bullet:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.speed = 10
+        self.speed = 5
         self.bild = sprite_bullet
 
 
-    def flytta(self):
+    def move(self):
         self.y = self.y - self.speed
 
-    def rita(self, skärm):
-        skärm.blit(self.bild, (self.x, self.y))
+    def draw(self, screen):
+        screen.blit(self.bild, (self.x, self.y))
 
 
 player_1 = Player1()
 player_2 = Player2()
 
+bullet_counter1 = 0
+bullet_counter2 = 0
 
-
-bullet_list = []
+bullet_list1 = []
+bullet_list2 = []
 while game:
     
     player_1.move()
@@ -121,8 +123,34 @@ while game:
             game = False
 
     screen.fill((0, 0, 0))
+    keys = pygame.key.get_pressed()
 
+    if keys[pygame.K_SPACE]:
+
+        bullet_list1.append(Bullet(player_1.player1_x + 20, player_1.player1_y))
+        bullet_counter1 = 0
+
+    if keys[pygame.K_KP_ENTER]:
+
+        bullet_list2.append(Bullet(player_2.player2_x + 20, player_2.player2_y))
+        bullet_counter2 = 0
+
+    for bullet in reversed(bullet_list1):
+        bullet.move()
+        bullet.draw(screen)
+
+        if bullet.y < -100:
+            bullet_list1.remove(bullet)
+
+    for bullet in reversed(bullet_list2):
+        bullet.move()
+        bullet.draw(screen)
+
+        if bullet.y < -100:
+            bullet_list2.remove(bullet)
     clock.tick(60)
+    bullet_counter1 = bullet_counter1 + 1
+    bullet_counter2 = bullet_counter2 + 1
     screen.blit(sprite_player1, (player_1.player1_x, player_1.player1_y))
     screen.blit(sprite_player2, (player_2.player2_x, player_2.player2_y))
     pygame.display.flip()
