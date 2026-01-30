4adc2cfbf0cd42f098565cf369968e6133af6fdb
Neo Östlund Zetterberg <neo.ostlundzetterbe@learnet.se>
2026-01-30 10:40:40 +0100

    I made a new button class for menues.

--- UI.py
+++ UI.py
@@ -1,12 +1,15 @@
-from typing import Optional, Protocol, runtime_checkable
+"""UI (user-interface) elements for menus and other..."""
+
+from typing import (
+    Callable, Optional, Protocol, runtime_checkable
+)
+from dataclasses import dataclass
 import pygame
 
 @runtime_checkable
 class Element(Protocol):
-    pos_x: int
-    pos_y: int
-    size_x: int
-    size_y: int
+    """Construction drawing for construction drawings"""
+    rect: pygame.Rect
 
     def handle_event(self, event: pygame.event.Event) -> None: ...
     def update(self, dt: float) -> None: ...
@@ -42,9 +45,59 @@ text = Text(font, "Hello!", (10, 10, 10)) # (r, g, b)
 text.render(surface, center=(10, 10))
 """
 
+@dataclass # Simpelt, består BARA data
+class ButtonConfig:
+    bg: tuple[int, int, int] # background
+    hover_bg: tuple[int, int, int]
+    pressed_bg: tuple[int, int, int]
+    border_color: tuple[int, int, int]
+    border_radius: int
+    border_width: int
+
+"""
+config = ButtonConfig(bg=(50, 50, 50), ...)
+print(config.bg) # -> (50, 50, 50)
+"""
+
 class Button(Element):
     def __init__(
         self,
-        text: Text
+        rect: pygame.Rect,
+        config: ButtonConfig,
+        text: Text,
+        on_press: Callable
     ) -> None:
-        ...
+        self.rect = rect
+        self.config = config
+        self.text = text
+        self.on_press = on_press
+
+        self.drawn_bg = config.bg
+        self.hover = False
+        self.pressed = False
+    
+    def handle_event(self, event: pygame.event.Event) -> None: 
+        match event.type:
+            case pygame.MOUSEMOTION:
+                self.hover = self.rect.collidepoint(event.pos)
+            case pygame.MOUSEBUTTONDOWN:
+                self.pressed = self.hover
+            case pygame.MOUSEBUTTONUP:
+                if self.pressed:
+                    self.on_press()
+                self.pressed = False
+            case pygame.WINDOWLEAVE:
+                self.pressed = False
+
+    def update(self, dt: float) -> None: 
+        if self.pressed:
+            self.drawn_bg = self.config.pressed_bg
+        elif self.hover:
+            self.drawn_bg = self.config.hover_bg
+        else:
+            self.drawn_bg = self.config.bg
+
+    def render(self, surface: pygame.Surface) -> None: 
+        pygame.draw.rect(surface, self.drawn_bg, self.rect, border_radius=self.config.border_radius)
+        pygame.draw.rect(surface, self.drawn_bg, self.rect, self.config.border_width, self.config.border_radius)
+        self.text.render(surface, center=self.rect.center)
