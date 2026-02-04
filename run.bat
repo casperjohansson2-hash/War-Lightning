9bb3e5c1f39fb51b5ad9fd3004fc6f65a55fa95a
Neo Östlund Zetterberg <neo.ostlundzetterbe@learnet.se>
2026-02-04 14:16:58 +0100

    Fixed the batch file.

--- run.bat
+++ run.bat
@@ -1,8 +1,15 @@
 @echo off 
 title War Lightning - Console
+:loop
 py main.py
 if %errorlevel% NEQ 0 (
     echo An error occured.
     pause >nul
+    exit /b 0
 )
-exit \b 0
+set /p key="Press Q to quit: "
+if /i "%key%"=="Q" goto end
+goto loop
+:end
+echo Exiting...
+exit /b 0
